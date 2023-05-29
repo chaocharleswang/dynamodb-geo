2256a3a2d0865cda58a57e57157bf2d171d8c636
Charles <chaocharleswang@gmail.com>
2023-05-29 09:06:57 -0700

    Add global secondary index implicitly

--- dynamodbgeo/util/GeoTableUtil.py
+++ dynamodbgeo/util/GeoTableUtil.py
@@ -33,27 +33,27 @@ class GeoTableUtil:
                 {'AttributeName': self.config.hashKeyAttributeName, 'AttributeType': 'N'},
                 {'AttributeName': self.config.rangeKeyAttributeName, 'AttributeType': 'S'},
                 {'AttributeName': self.config.geohashAttributeName, 'AttributeType': 'N'},
-                # {'AttributeName': 'state', 'AttributeType': 'S'},
-                # {'AttributeName': 'city', 'AttributeType': 'S'},
+                {'AttributeName': 'state', 'AttributeType': 'S'},
+                {'AttributeName': 'city', 'AttributeType': 'S'}
             ],
-            # 'GlobalSecondaryIndexes': [
-            #     {
-            #         'IndexName': 'state-city-index',
-            #         'KeySchema': [
-            #             {
-            #                 'KeyType': 'HASH',
-            #                 'AttributeName': 'state'
-            #             },
-            #             {
-            #                 'KeyType': 'RANGE',
-            #                 'AttributeName': 'city'
-            #             }
-            #         ],
-            #         'Projection': {
-            #             'ProjectionType': 'ALL'
-            #         }
-            #     }
-            # ],
+            'GlobalSecondaryIndexes': [
+                {
+                    'IndexName': 'state-city-index',
+                    'KeySchema': [
+                        {
+                            'KeyType': 'HASH',
+                            'AttributeName': 'state'
+                        },
+                        {
+                            'KeyType': 'RANGE',
+                            'AttributeName': 'city'
+                        }
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'ALL'
+                    }
+                }
+            ],
             'LocalSecondaryIndexes': [
                 {
                     'IndexName': self.config.geohashIndexName,
